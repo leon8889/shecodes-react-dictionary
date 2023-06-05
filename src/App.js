8793d4a765114895da8825da1502c74d94b409e2
leon8889 <anna.kuhn89@gmx.de>
2023-06-05 10:35:55 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
+
+import Dictionary from "./components/Dictionary.js";
 
 // https://github.com/leon8889/shecodes-react-dictionary
 // https://relaxed-mochi-9c5a9e.netlify.app/
@@ -9,24 +11,11 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					href="https://getbootstrap.com/docs/3.4/css/"
-					className="btn btn-primary"
-				>
-					Bootstrap
-				</a>
 			</header>
+			<main>
+				<Dictionary />
+			</main>
+			<footer>coded by leon</footer>
 		</div>
 	);
 }
---
+++ src/components/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+	let [keyword, setKeyword] = useState("");
+
+	function search(event) {
+		event.preventDefault();
+
+		alert(`Searching for ${keyword}`);
+	}
+
+	function handleKeyWordChange(event) {
+		setKeyword(event.target.value);
+	}
+
+	return (
+		<div className="Dictionary">
+			<form onSubmit={search}>
+				<input type="search" autoFocus={true} onChange={handleKeyWordChange} />
+				<input type="submit" value="Search" />
+			</form>
+		</div>
+	);
+}
