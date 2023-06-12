817ea5ab36d2480de604c86e86be6e2c7e2a48e7
leon8889 <anna.kuhn89@gmx.de>
2023-06-12 16:21:24 +0200

    added synonyms

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Synonyms from "./Synonyms.js";
+
 export default function Meaning(props) {
 	if (props.meaning) {
 		return (
@@ -7,6 +9,7 @@ export default function Meaning(props) {
 				<h3>{props.meaning.partOfSpeech}</h3>
 				<p className="definition">{props.meaning.definition}</p>
 				<p className="example">{props.meaning.example}</p>
+				<Synonyms synonyms={props.meaning.synonyms} />
 			</div>
 		);
 	} else {
---
+++ src/components/Synonyms.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Synonyms(props) {
+	if (props.synonyms) {
+		return (
+			<div className="Synonyms">
+				<ul className="inline-list">
+					<li className="inline-list-item inline-list-item-first">Synonyms:</li>
+					{props.synonyms.map(function (synonym, index) {
+						return (
+							<li className="inline-list-item" key={index}>
+								{synonym}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	} else {
+		return null;
+	}
+}
